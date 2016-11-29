a53dbde2eb11874604c1deefefef3a2a6d60b92d
lydiskim <lydiskim@gmail.com>
2016-11-29 11:12:51 +0900

    Update Touchsensor.java

--- Touchsensor.java
+++ Touchsensor.java
@@ -1,10 +1,12 @@
-package Touch;
+package fighting_robot;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.AbstractFilter;
 import lejos.hardware.Sound;
 
 public class Touchsensor extends AbstractFilter{
 	float[] sample;
+	public static final boolean TOUCHED = true;
+	public static final boolean UNTOUCHED = false;
 	
 	public Touchsensor(SampleProvider source){
 		super(source);
