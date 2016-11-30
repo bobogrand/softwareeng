4734a8c6d827d7f7e6ec5ffa2926f8367b04af66
yunmin1130 <yunmin1992@sogang.ac.kr>
2016-11-30 11:49:26 +0900

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -12,6 +12,7 @@ import lejos.hardware.motor.Motor;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.NXTLightSensor;
+import lejos.hardware.sensor.NXTUltrasonicSensor;
 import lejos.remote.ev3.RemoteEV3;
 
 
@@ -24,6 +25,9 @@ import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.sensor.NXTLightSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
 
+import lejos.utility.Stopwatch;
+
+import java.util.Random;
 
 public class Robot {
 		
@@ -33,12 +37,15 @@ public class Robot {
 		public static final int ROTATE = 2;
 		public static final int TURN_LEFT = 3;
 		public static final int TURN_RIGHT = 4;
-		public static final int RANDOM = 5;
+		public static final int RANDOM = 7;
 		
 		public static final boolean BLACK = true;
 		public static final boolean WHITE = false;
 		public static final boolean TOUCHED = true;
 		public static final boolean UNTOUCHED = false;
+		
+		public static final boolean D = true;
+		public static final boolean A = false;
 	
 		RemoteEV3 ev3;	
 		/** motor part */
@@ -47,52 +54,116 @@ public class Robot {
 		
 		// now state
 		public static DifferentialPilot pilot; 
-		public static int speed;
+		public static double speed;
 		//public static boolean line;
 		public static Lightsensor Light;
 		
 
+		static Lightsensor light;
+		static Touchsensor touch;
+		static Sonarsensor sonar_NXT;
+		static Sonarsensor sonar_EV3;
+		
+		static Brick b;
+		
+		static Port s1;
+		static Port s2;
+		static Port s3;
+		static Port s4;
+		
+		static Stopwatch timer;
 		/** test case */
 		
-		public static void test() {
-			
-			Lightsensor light;
-			Touchsensor touch;
-			
-			// TODO Auto-generated method stub
-			Brick b = BrickFinder.getDefault();
-			Port s1 = b.getPort("S1");
-			Port s2 = b.getPort("S2");
-			//Port s4 = b.getPort("S4");
-			NXTLightSensor NXTlight = new NXTLightSensor(s1);
-			//EV3UltrasonicSensor EV3sonar = new EV3UltrasonicSensor(s4);
-			EV3TouchSensor EV3touch = new EV3TouchSensor(s2);
-			
-			//sonar = new Ultrasonic(EV3sonar.getMode("Distance"));
-			touch = new Touchsensor(EV3touch);
-			light = new Lightsensor(NXTlight);
-			
-			//if(sonar.distance() < 0.45){
-			//}
-			if(touch.pressed()== TOUCHED){
-				Sound.twoBeeps();
-				sleep(2000);
+		public static void test(boolean flag) {
+			if(flag == D) {
+				while(true) {
+					if(timer.elapsed() >= 30000)
+						break;
+					rotate(100);
+					motor(FORWARD,0);
+					sleep(1000);
+				}
 			}
-			if(light.pressed()== WHITE){
-				Sound.twoBeeps();
-				sleep(2000);
-			}
-				
 		}
-		
-		/*end tesr**/
-		
-		
+		public static void return_back(){
+			int elapsed_time;
+			//motor(BACKWARD,0);
+			motor(ROTATE,70);
+			motor(FORWARD,0);
+			int start_time = timer.elapsed();
+			while(true){
+				elapsed_time = timer.elapsed();
+				if((elapsed_time - start_time) > 800){
+					Sound.beep();
+					pilot.stop();
+					motor(ROTATE,60);
+					motor(FORWARD,0);
+					start_time = timer.elapsed();
+				}
+				if(light.get_light() == BLACK){
+					Sound.twoBeeps();
+					break;
+				}
+			}
+			sleep(100);
+		}
+		public static void rotate(int speed){
+			pilot.setRotateSpeed(speed);
+			pilot.rotateLeft();
+			while(true){
+				if(light.get_light() == WHITE){
+					Sound.beep();
+					return_back();
+					pilot.setRotateSpeed(speed);
+					pilot.rotateLeft();
+				}
+				if(sonar_EV3.distance() <=0.50){
+					DEF();
+					Sound.buzz();
+					pilot.stop();
+					break;
+				}
+				if(sonar_NXT.distance() <= 0.40){
+					DEF();
+					Sound.buzz();
+					pilot.stop();
+					break;
+				}
+			}
+		}
+		public static void DEF(){
+			if(sonar_EV3.distance() <=0.40){
+				motor(7,0);
+				sleep(400);					
+			}	
+		}
+		/*end tesr**/		
 		// init
 	public static void init(){
 		// set speed
-		speed = 150;
 		pilot = new DifferentialPilot(1.5f,6,Left,Right);	//POINT
+		// TODO Auto-generated method stub
+		b = BrickFinder.getDefault();
+		s1 = b.getPort("S1");
+		s2 = b.getPort("S2");
+		s3 = b.getPort("S3");
+		s4 = b.getPort("S4");
+		NXTLightSensor NXTlight = new NXTLightSensor(s1);
+		EV3TouchSensor EV3touch = new EV3TouchSensor(s2);
+		NXTUltrasonicSensor NXTsonar = new NXTUltrasonicSensor(s3);
+		EV3UltrasonicSensor EV3sonar = new EV3UltrasonicSensor(s4);
+		
+		sonar_NXT = new Sonarsensor(NXTsonar.getMode("Distance"));
+		sonar_EV3 = new Sonarsensor(EV3sonar.getMode("Distance"));
+		touch = new Touchsensor(EV3touch);
+		light = new Lightsensor(NXTlight);
+		
+		//if(sonar.distance() < 0.45){
+		//}
+		
+		timer = new Stopwatch();
+		timer.reset();
+		speed = pilot.getMaxTravelSpeed();
 		return;
 	}
 		
@@ -100,8 +171,9 @@ public class Robot {
 	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException  {
 		
 		init();
-
-		test();
+		
+		//return_back();
+		test(D);
 		return;
 		
 	}
@@ -124,20 +196,20 @@ public class Robot {
 			break;
 		//turn left
 		case TURN_LEFT:
-			pilot.arc(5, -90);
+			pilot.arc(-3, -angle);
 			break;
 		//turn right
 		case TURN_RIGHT:
-			pilot.arc(5, 90);
+			pilot.arc(3, angle);
 			break;
-			
 		case RANDOM:
-			
-			
+			Random rand = new Random();
+			int a = rand.nextInt(6);
+			int b = rand.nextInt(200);
+			a -=3; b-=100;
+			pilot.arc(a, b);
 			break;
 		}
-
-		
 	}
 	
 	
@@ -145,7 +217,7 @@ public class Robot {
 	
 	public static void sleep(int time){
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(time);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
